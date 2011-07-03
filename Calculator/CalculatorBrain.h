a18c0a79289bd283bf080028cdcac5a81128b104
MiDaList <midalist@gmail.com>
2011-07-03 22:27:47 +0200

    Add CalculatorBrain Class

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Michał Listowski on 11-07-03.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CalculatorBrain : NSObject {
+    
+}
+
+@end
---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,14 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Michał Listowski on 11-07-03.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+
+@implementation CalculatorBrain
+
+@end
